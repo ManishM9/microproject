7c0b51d86063de76f8600c60e1d286e5cb3d35b4
ManishM9 <mr.malepati@gmail.com>
2018-10-17 11:52:35 +0530

    Completed order taking

--- server.js
+++ server.js
@@ -87,6 +87,25 @@ app.get("/table1/:item", (req, res) => {
 				res.send("Added Desert!");
 			}
 		});
+	} else if(item === 5){
+		Table1.find({}, (err, docs) => {
+			if(err){
+				console.log(err);
+				throw err;
+			} else {
+				var response = "Checkout completed: " + docs.length + " items. Please pay at the counter :)";
+				Table1.deleteMany({}, (err, info) => {
+					if(err){
+						console.log(err);
+						throw err;
+					} else {
+						console.log(info);
+						res.send(response);
+					}
+				});
+				// res.send(response);
+			}
+		});
 	} else {
 		res.send("INVALID!!!!   ~ SERVER.")
 	}
@@ -95,7 +114,7 @@ app.get("/table1/:item", (req, res) => {
 app.get("/table2/:item", (req, res) => {
 	var item = req.params.item;
 	if(item === 1){
-		Table1.create({ tableno: 2, item: 1, itemName: "Pizza" }, (err, doc) => {
+		Table2.create({ tableno: 2, item: 1, itemName: "Pizza" }, (err, doc) => {
 			if(err){
 				console.log(err);
 				throw err;
@@ -105,7 +124,7 @@ app.get("/table2/:item", (req, res) => {
 			}
 		});
 	} else if(item === 2){
-		Table1.create({ tableno: 2, item: 2, itemName: "Burger" }, (err, doc) => {
+		Table2.create({ tableno: 2, item: 2, itemName: "Burger" }, (err, doc) => {
 			if(err){
 				console.log(err);
 				throw err;
@@ -115,7 +134,7 @@ app.get("/table2/:item", (req, res) => {
 			}
 		});
 	} else if(item === 3){
-		Table1.create({ tableno: 2, item: 3, itemName: "Soft Drink" }, (err, doc) => {
+		Table2.create({ tableno: 2, item: 3, itemName: "Soft Drink" }, (err, doc) => {
 			if(err){
 				console.log(err);
 				throw err;
@@ -125,13 +144,32 @@ app.get("/table2/:item", (req, res) => {
 			}
 		});
 	} else if(item === 4){
-		Table1.create({ tableno: 2, item: 4, itemName: "Desert" }, (err, doc) => {
+		Table2.create({ tableno: 2, item: 4, itemName: "Desert" }, (err, doc) => {
 			if(err){
 				console.log(err);
 				throw err;
 			} else {
 				console.log(doc);
 				res.send("Added Desert!");
+			}
+		});
+	} else if(item === 5){
+		Table2.find({}, (err, docs) => {
+			if(err){
+				console.log(err);
+				throw err;
+			} else {
+				var response = "Checkout completed: " + docs.length + " items. Please pay at the counter :)";
+				Table2.deleteMany({}, (err, info) => {
+					if(err){
+						console.log(err);
+						throw err;
+					} else {
+						console.log(info);
+						res.send(response);
+					}
+				});
+				// res.send(response);
 			}
 		});
 	} else {
